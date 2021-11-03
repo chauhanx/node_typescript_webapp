049e6440b929028d703062bfff625d362c0339bc
ankur <chauhanankur94@gmail.com>
2021-11-03 15:32:25 -0400

    Changes fdon w

--- codedeploy/afterInstall.sh
+++ codedeploy/afterInstall.sh
@@ -1,1 +1,11 @@
 #!/bin/sh
+
+cd /home/ubuntu
+pwd
+mkdir l_webapp
+mv webapp /home/ubuntu/l_webapp/
+pwd
+cd /home/ubuntu/l_webapp/webapp
+pwd
+npm install
+npm test
--- codedeploy/applicationStart.sh
+++ codedeploy/applicationStart.sh
@@ -1,1 +1,5 @@
 #!/bin/sh
+
+cd /home/ubuntu/l_webapp/webapp
+sudo kill -9 $(sudo lsof -t -i:3000)
+npm run prod
