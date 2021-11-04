2c9b3717b43d355d45b4bbb62341e1bea8c8679d
ankur <chauhanankur94@gmail.com>
2021-11-04 13:10:31 -0400

    Changes

--- codedeploy/afterInstall.sh
+++ codedeploy/afterInstall.sh
@@ -9,5 +9,5 @@ pwd
 cd /home/ubuntu/webapp
 source /etc/profile
 sudo npm install
-sudo npm install nodemon -g
-sudo nohup nodemon >> debug.log 2>&1 &
+# sudo npm install nodemon -g
+# sudo nohup nodemon >> debug.log 2>&1 &
