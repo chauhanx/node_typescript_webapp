6f5d8d654a51056d5a6e48a33b44dfd0fa93ce31
ankur <chauhanankur94@gmail.com>
2021-11-03 23:55:50 -0400

    Changes

--- codedeploy/afterInstall.sh
+++ codedeploy/afterInstall.sh
@@ -10,6 +10,7 @@ cd /home/ubuntu/webapp
 source /etc/profile
 
 touch .env
+ls -la
 echo PORT=$PORT>>.env
 echo DB_HOST=$DB_HOST>>.env
 echo DB_NAME=$DB_NAME>>.env
@@ -19,8 +20,4 @@ echo SECRET_KEY=$SECRET_KEY>>.env
 echo DB_DIALECT $DB_DIALECT>>.env
 echo BASIC_AUTH_USER=$BASIC_AUTH_USER>>.env
 echo BASIC_AUTH_PASS=$BASIC_AUTH_PASS>>.env
-
-
 sudo npm install
-ls -la
-cat .env
