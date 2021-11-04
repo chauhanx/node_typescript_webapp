026b5337e02d08c16ee62d29e49866f23f7e0134
ankur <chauhanankur94@gmail.com>
2021-11-04 15:41:10 -0400

    Changes

--- codedeploy/applicationStart.sh
+++ codedeploy/applicationStart.sh
@@ -3,8 +3,8 @@ cd /home/ubuntu/webapp
 # sudo kill -9 $(sudo lsof -t -i:3000)
 ls -a
 source /etc/profile
-# sudo pm2 install typescript
-# sudo pm2 install ts-node
-echo **************************************************************
+sudo pm2 install typescript
+sudo pm2 install ts-node
+echo "**************************************************************"
 ls -a
 npm run prod
