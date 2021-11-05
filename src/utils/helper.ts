43baf51231d440569aff9481ee13288782aa7492
ankur <chauhanankur94@gmail.com>
2021-11-05 12:53:05 -0400

    Changes

--- src/utils/helper.ts
+++ src/utils/helper.ts
@@ -61,7 +61,7 @@ export const upload_s3 = async(file) => {
         
         const params = {
             Bucket: `${appConfigs.s3.BUCKET_NAME}`,
-            Key: fileName,
+            Key: `${fileName}.${file.mime}`,
             Body: new Buffer(file.data,'base64'),
             ContentEncoding: 'base64',
             ContentType: file.mime
@@ -104,30 +104,3 @@ export const delete_s3 = async(key) => {
     }
      
 }
-
-// function deleteFromS3(file) {
-//     let s3bucket = new AWS.S3({
-//       accessKeyId: IAM_USER_KEY,
-//       secretAccessKey: IAM_USER_SECRET,
-//       Bucket: BUCKET_NAME
-//     });
-//     s3bucket.deleteObject({
-//           Bucket: BUCKET_NAME,
-//           Key: file//.name//,
-//           //Body: file.data
-//         },function (err,data){
-//           if(err){
-//             console.log(err);
-//           }
-//           console.log(data);
-//         })
-//         // s3bucket.upload(params, function (err, data) {
-//         //   if (err) {
-//         //     console.log('error in callback');
-//         //     console.log(err);
-//         //   }
-//         //   console.log('success'+'--------------->>>>>');
-//         //   console.log(data.Location);
-//         // });
-//    // });
-//   }
