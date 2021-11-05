845a552b2751b4bbad131ba31335f51cc7a831ab
ankur <chauhanankur94@gmail.com>
2021-11-05 12:11:55 -0400

    Changes

--- codedeploy/afterInstall.sh
+++ codedeploy/afterInstall.sh
@@ -6,11 +6,13 @@ ls -a
 sudo unzip webapp.zip
 pwd
 cd /home/ubuntu/webapp
-source /etc/profile
 sudo npm install
-sudo npm install -g typescript
-sudo npm install -g ts-node
+# sudo npm i pm2
+# sudo ./node_modules/.bin/pm2 install typescript
+# sudo npm install -g typescript
+# sudo npm install -g ts-node
 echo "**************************************************************"
-
-# sudo npm install nodemon -g
-# sudo nohup nodemon >> debug.log 2>&1 &
+# mkdir /home/ubuntu/logs
+sudo npm install nodemon -g
+sudo nohup nodemon >> debug.log 2>&1 &
+sudo apt install node-typescript
--- src/app.ts
+++ src/app.ts
@@ -4,11 +4,9 @@ import cors from 'cors';
 import helmet from 'helmet';
 import routes from './routes';
 import morgan from 'morgan';
-import db from './config/db.config';
+import dbConfig from './config/db.config';
 import path from 'path';
 import bodyParser from 'body-parser';
-// import swaggerUI from 'swagger-ui-express';
-// import swDocument from '../swagger.def'
 
 dotenv.config();
 
@@ -25,16 +23,37 @@ const PORT =  process.env.PORT || 3000;
 console.log("**************************************");
 
 // configure database connectivity
-db.config();
+dbConfig.config()
+// db.sequelize
+// .authenticate()
+// // .sync({force: true})
+// .then(async() => {
+//     console.log("sycn doneß");
+//     console.error("database connected successfully!");
+//     // try {
+//         await db.sequelize.sync({force: true})
+//         // await db.sequelize.sync();
+//         // console.log("sycn doneß");
+    app.listen(PORT, () => {
+        console.log('The application is listening on port 3000!');
+    })
+//     // } catch (error) {
+//     //     console.log("***************** error ***************************");
+//     //     console.log(error);
+//     // }
+
+//   }).catch( (e: any) => {
+//     console.log("***************** error ***************************");
+//     console.error("database connection error!");
+//       console.log(e.stack)
+//   });
+
+
 // use middlewares
 app.use(helmet());
 app.use(cors());
 app.use(express.json({ limit: '5mb' }));
-// app.use('/docs',swaggerUI.serve,swaggerUI.setup(swDocument));
 app.use(morgan('dev'));
 app.use(routes);
 
 
-app.listen(PORT, () => {
-    console.log('The application is listening on port 3000!');
-})
