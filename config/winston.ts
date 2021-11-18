301992fea47251b43569ed8b8c5a63fc82a60cad
ankur <chauhanankur94@gmail.com>
2021-11-18 13:03:57 -0500

    Changes

--- config/winston.ts
+++ config/winston.ts
@@ -1,9 +1,11 @@
-import appRoot from 'app-root-path';
-import winston from 'winston';
+// import appRoot from 'app-root-path';
+// import winston from 'winston';
+
+import {  appConfigs }  from '../src/config/config';
 var options = {
     file: {
         // level: 'info',
-        filename: `${appRoot}/logs/csye6225.log`
+        // filename: `${appRoot}/logs/csye6225.log`
         // handleExceptions: true,
         // json: true,
         // maxsize: 4194304,
@@ -14,33 +16,91 @@ var options = {
         level: 'debug',
         handleExceptions: true,
         json: false,
-        colorize: true
+        colorize: true,
+        timestamp:true
     },
 };
 
-const logFormat = winston.format.combine(
-    winston.format.timestamp(),
-    winston.format.align(),
-    winston.format.printf(
-        info => `LABEL: ${info.label}, LEVEL:  ${info.level},TIMESTAMP: ${info.timestamp}, MESSAGE: ${info.message}`
-    )
-);
+// const logFormat = winston.format.combine(
+//     winston.format.timestamp(),
+//     winston.format.align(),
+//     winston.format.printf(
+//         info => `LABEL: ${info.label}, LEVEL:  ${info.level},TIMESTAMP: ${info.timestamp}, MESSAGE: ${info.message}`
+//     )
+// );
+
+// export const logger =  winston.createLogger({
+//     format: logFormat,
+//     transports: [
+//         new winston.transports.File({filename:`logs/csye6225.log`}),
+//         new winston.transports.Console(options.console)
+//     ],
+//     exitOnError: false,
+// });
+
+
+// logger.stream({
+//     write: (message, encoding) =>{
+//         logger.info(message); 
+//     }
+// });
+
+
+// module.exports = logger;
+
+
+import winston from 'winston';
+import CloudWatchTransport from 'winston-aws-cloudwatch';
 
 export const logger =  winston.createLogger({
-    format: logFormat,
-    transports: [
-        new winston.transports.File({filename:`logs/csye6225.log`}),
-        new winston.transports.Console(options.console)
-    ],
-    exitOnError: false,
+  transports: [
+    // new (winston.transports.Console)({
+    //   timestamp: true,
+    //   colorize: true
+    // })
+    new CloudWatchTransport({
+        logGroupName: 'csye6225', 
+        logStreamName: 'webapp',
+        createLogGroup: true,
+        createLogStream: true,
+        submissionInterval: 200,
+        submissionRetryCount: 1,
+        batchSize: 5,
+        awsConfig: {
+            accessKeyId: `${appConfigs.s3.ACCESS_KEY}`,
+            secretAccessKey: `${appConfigs.s3.SECRET_KEY}`,
+            region: process.env.CLOUDWATCH_REGION
+        },
+        formatLog: item =>
+          `${item.level}: ${item.message} ${JSON.stringify(item.meta)}`
+    }),
+    new winston.transports.Console(options.console)
+  ]
 });
 
+// var config = {
+//   logGroupName: 'csye6225',
+//   logStreamName: 'webapp',
+//   createLogGroup: false,
+//   createLogStream: true,
+//   awsConfig: {
+//     accessKeyId: process.env.CLOUDWATCH_ACCESS_KEY_ID,
+//     secretAccessKey: process.env.CLOUDWATCH_SECRET_ACCESS_KEY,
+//     region: process.env.CLOUDWATCH_REGION
+//   },
+//   formatLog: function (item) {
+//     return item.level + ': ' + item.message + ' ' + JSON.stringify(item.meta)
+//   }
+// }
+
+// logger.add(CloudWatchTransport, config);
+
+logger.level = process.env.LOG_LEVEL || "silly";
 
 logger.stream({
-    write: (message, encoding) =>{
-        logger.info(message); 
-    }
+  write: function(message, encoding) {
+    logger.info(message);
+  }
 });
-
 
 // module.exports = logger;
