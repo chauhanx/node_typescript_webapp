9869bf51a3df3caace66089918ed7492e051a093
ankur <chauhanankur94@gmail.com>
2021-11-18 12:21:23 -0500

    Changes

--- codedeploy/applicationStart.sh
+++ codedeploy/applicationStart.sh
@@ -5,7 +5,7 @@ ls -a
 source /etc/profile
 ls -a
 /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a fetch-config -m ec2 -c file:/home/ubuntu/webapp/cloudwatch-config.json -s
-sudo chmod 755 /home/ubuntu/webapp/logs/csye6225.log
+sudo chmod 775 /home/ubuntu/webapp/logs/csye6225.log
 pm2 start dist/src/app.js
 pm2 startup systemd
 sudo env PATH=$PATH:/usr/bin /usr/lib/node_modules/pm2/bin/pm2 startup systemd -u ubuntu --hp /home/ubuntu
