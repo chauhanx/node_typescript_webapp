b05ea893b15179c46e6505f5688e971ebb707699
ankur <chauhanankur94@gmail.com>
2021-11-21 13:51:43 -0500

    Chnages

--- codedeploy/applicationStop.sh
+++ codedeploy/applicationStop.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -m ec2 -a stop || true
+# sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -m ec2 -a stop || true
